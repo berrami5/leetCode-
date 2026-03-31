327820b892d34181c6e2047e55b34e79fa4036fa
User 01 <75183951+berrami5@users.noreply.github.com>
2026-03-31 18:03:25 +0100

    Time: 3 ms (45.13%) | Memory: 8.5 MB (91.34%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string str = to_string(x);
-        int i = 0;
-        const int length = str.length() - 1; 
-        while (i  < length - i) {
-            if (str[i] != str[length - i]) {
-                return 0;
-            }
-            i++;
+        if (x == 0)return 1;
+        long int resivedX = 0;
+        int testX = x;
+        while (testX) {
+            resivedX *= 10;
+            resivedX += testX % 10;
+            testX /= 10;
         }
-        return 1;
+        
+        return resivedX == x && x > 0;
     }
 };
